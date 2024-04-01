88e0e8775b2cd8645ccf670876df9a99ab173725
anthony <anthonythardman@gmail.com>
2024-04-01 15:18:45 -0600

    Add IActorBridge dependency to ClientService constructor

--- actorSystem/Services/ClientService.cs
+++ actorSystem/Services/ClientService.cs
@@ -6,13 +6,16 @@ namespace actorSystem.Services;
 public class ClientService : IClientService
 {
   private readonly HubConnection _hubConnection;
+  private readonly IActorBridge _akkaService;
 
-  public ClientService()
+  public ClientService(IActorBridge akkaService)
   {
     _hubConnection = new HubConnectionBuilder()
         .WithUrl(Environment.GetEnvironmentVariable("SIGNALR_URL") ?? "http://asteroids_signalr:8080/ws")
         .WithAutomaticReconnect()
         .Build();
+    
+    _akkaService = akkaService;
   }
 
   public async Task ConnectAsync()

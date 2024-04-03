021ab64cc72f30114ce433c962ecd459a4070b8c
Bridger Thompson <bridgerat00@gmail.com>
2024-04-03 08:01:39 -0600

    client connects to websocket

--- signalR/Program.cs
+++ signalR/Program.cs
@@ -2,31 +2,24 @@ using signalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("http://localhost:8080") // Add any other origins your client might use
               .AllowAnyMethod()
-              .AllowAnyHeader());
+              .AllowAnyHeader()
+              .AllowCredentials()); // Allows credentials like cookies, authorization headers, etc.
 });
+
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-app.MapHub<AsteroidsHub>("/ws");
 app.UseCors("AllowAll");
+app.MapHub<AsteroidsHub>("/ws");
 
 app.Run();

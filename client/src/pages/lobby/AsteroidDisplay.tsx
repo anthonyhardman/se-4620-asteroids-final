b6f67c5f6ef0f84bcf4c5f1a79f074a3c96ee648
Bridger Thompson <bridgerat00@gmail.com>
2024-04-10 12:08:46 -0600

    linting

--- client/src/pages/lobby/AsteroidDisplay.tsx
+++ client/src/pages/lobby/AsteroidDisplay.tsx
@@ -1,5 +1,5 @@
-import { FC, useMemo } from "react";
-import { Vector3, Quaternion, BufferGeometry, BufferAttribute } from "three";
+import { FC } from "react";
+import { Vector3, Quaternion } from "three";
 import { Asteroid } from "../../models/Lobby";
 
 interface AsteroidDisplayProps {
@@ -10,21 +10,8 @@ interface AsteroidDisplayProps {
 export const AsteroidDisplay: FC<AsteroidDisplayProps> = ({ asteroid, model }) => {
   const scaleFactor = 50; // Adjust this factor to scale the asteroid size up
 
-  const asteroidGeometry = useMemo(() => {
-    const geometry = new BufferGeometry();
-    const vertices = new Float32Array([
-      0, 1, 0,   // Tip of the asteroid
-      -1, -1, 0, // Left base
-      1, -1, 0   // Right base
-    ]);
-    geometry.setAttribute('position', new BufferAttribute(vertices, 3));
-    return geometry;
-  }, []);
-
   const direction = new Vector3(asteroid.direction.x, asteroid.direction.y, 0);
   const quat = new Quaternion().setFromUnitVectors(new Vector3(0, 1, 0), direction.normalize());
-  const healthPercentage = asteroid.health / (asteroid.size * asteroid.size);
-  const color = healthPercentage > 0.5 ? "green" : "red";
 
   return (
     <group position={[asteroid.position.x, asteroid.position.y, 0]} scale={[scaleFactor * asteroid.size, scaleFactor * asteroid.size, scaleFactor * asteroid.size]}>
